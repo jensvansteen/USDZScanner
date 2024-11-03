215a7d5edcdd79c8e373e94485a0d2e0f605ddec
Jens <33849126+jensvansteen@users.noreply.github.com>
2024-11-03 08:28:12 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     targets: [
         .target(
             name: "USDZScanner",
-            resources: [.copy("Resources/")])
+            resources: [.process("Resources/")])
     ]
 )
